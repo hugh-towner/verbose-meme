7c281aea186a89d5bd7c4cd63ce3dc0acdbff23d
zidl <twogr20@gmail.com>
2021-01-22 18:58:09 +0000

    Finished player movement

--- verbose-meme/Player.gd
+++ verbose-meme/Player.gd
@@ -1,30 +1,33 @@
 extends KinematicBody2D
 
-var torch = preload("res://Torch.tscn")
-var movespeed = 180
+const torch = preload("res://Torch.tscn")
+
+const FRICTION = 1500
+const MAX_SPEED = 100
+const ACCELERATION = 900
+
+var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
-	var motion = Vector2()
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	input_vector = input_vector.normalized()
 
-	if Input.is_action_pressed("up"):
-		animationPlayer.play("WalkUp")
-		motion.y -= 1
-	elif Input.is_action_pressed("down"):
-		animationPlayer.play("WalkDown")
-		motion.y += 1
-	elif Input.is_action_pressed("right"):
-		animationPlayer.play("WalkRight")
-		motion.x += 1
-	elif Input.is_action_pressed("left"):
-		animationPlayer.play("WalkLeft")
-		motion.x -= 1
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("IdleFront")
-	
-	motion = motion.normalized()
-	motion = move_and_slide(motion * movespeed)
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+	velocity = move_and_slide(velocity)
 	
 	if Input.is_action_just_pressed("LMB"):
 		place_torch()
